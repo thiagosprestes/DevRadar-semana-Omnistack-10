f640873e806d347e57ca4e279a6ba16ee09e0463
thiagosprestes <thiago.santos.prestes@gmail.com>
2020-01-17 12:38:31 -0300

    Adicionando funcionalidade que permite encontrar devs em tempo real, ou seja, no momento em que eles se cadastram na aplicação através de websockets, finalizando o projeto.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 
 const routes = require('./routes')
 
+const { setupWebsocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.connect('mongodb+srv://omnistack:semanaomnistack10@cluster0-ddq1s.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
